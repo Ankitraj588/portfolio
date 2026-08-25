7d3afaa4a2a996eb96d417cae84be6ee8c45d2ba
Ankitraj <175405856+Ankitraj588@users.noreply.github.com>
2026-08-25 19:20:36 +0530

    Fix project configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 
-import {Route,Routes} from 'react-router-dom'
-import Home from './pages/Home/Home'
+import { Routes, Route } from 'react-router-dom'
 
+import Home from './pages/Home/Home'
 import Contact from './pages/Contact/Contact'
-import About  from './pages/About'
+import About from './pages/About'
 import Navbar from './pages/Navbar/Navbar'
 import NotFound from './pages/NotFound/NotFound'
 import Projects from './pages/Project/Projects'
@@ -13,29 +12,47 @@ import Blogs from './pages/Blog/Blog'
 import BlogDetails from './pages/Blog/BlogDetails'
 import Register from './pages/Auth/Register'
 import Login from './pages/Auth/Login'
+
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    < div className='flex flex-col  h-screen ' >
-  
- <Navbar />
+    <div className="flex flex-col h-screen">
 
+      <Navbar />
 
+      <Routes>
 
-<Routes>
+        <Route path="/" element={<Home />} />
 
-  <Route path='/' element={<Home/>}/>
-  <Route path='/about' element={<About/>}/>
-  <Route path='/contact' element={<Contact/>}/>
-  <Route path='/projects' element={<Projects/>}/>
-  <Route path='/blog' element={<Blogs/>}/>
-  <Route path="/blog/:id" element={<BlogDetails />} />
-  <Route path="/register" element={<Register />} />
-  <Route path="/login" element={<Login />} />
-  <Route path="/register" element={<Register />} />
-  <Route path='*' element={<NotFound/>}/>
-</Routes>
+        <Route path="/about" element={<About />} />
+
+        <Route path="/contact" element={<Contact />} />
+
+        <Route path="/projects" element={<Projects />} />
+
+        <Route path="/blog" element={<Blogs />} />
+
+        <Route
+          path="/blog/:id"
+          element={<BlogDetails />}
+        />
+
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+
+      </Routes>
+
     </div>
   )
 }
