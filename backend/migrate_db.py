aba0d126753b4da49c2c96e45eba31a5f7d5a336
Ankitraj <175405856+Ankitraj588@users.noreply.github.com>
2026-08-26 03:35:22 +0530

    Add database migration setup

---
+++ backend/migrate_db.py
@@ -0,0 +1,10 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
+django.setup()
+
+from django.core.management import call_command
+
+call_command("migrate")
