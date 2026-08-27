3674f6809dfac9305495e194f73b31b3731ad683
Ankitraj <175405856+Ankitraj588@users.noreply.github.com>
2026-08-27 06:33:58 +0530

    fixed the current api

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL =
   import.meta.env.VITE_API_URL ||
-  'https://portfolio-blogging-django-498yjpais.vercel.app'
+  'https://portfolio-blogging-django-ii7yt7h8p.vercel.app'
